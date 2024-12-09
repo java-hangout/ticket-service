608edb8e0c02d02236e7bb9dc2a96ba9aba6505a
java-hangout <contacts.veeresh@gmail.com>
2024-12-09 12:39:12 +0530

    code  update

--- src/main/java/com/kt/ts/ticketservice/controller/TicketController.java
+++ src/main/java/com/kt/ts/ticketservice/controller/TicketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tickets")
+@RequestMapping("/ticket")
 public class TicketController {
 
     @Autowired
